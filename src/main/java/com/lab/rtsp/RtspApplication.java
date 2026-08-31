349d1abc4ff71c9e44f838bbc804aabfa75da9f1
tangdesam <tangdesam@gmail.com>
2026-08-31 15:04:07 +0700

    chore: initial project setup

---
+++ src/main/java/com/lab/rtsp/RtspApplication.java
@@ -0,0 +1,13 @@
+package com.lab.rtsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtspApplication.class, args);
+	}
+
+}
